96a783f84d99410346be5616cf14bdb1dde979de
overetou <overetou@student.42.fr>
2018-03-21 17:26:26 +0100

    binary writing ok

---
+++ srcs/writing.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   writing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: overetou <overetou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/21 15:58:53 by overetou          #+#    #+#             */
+/*   Updated: 2018/03/21 15:58:54 by overetou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void    write_in_hexa(int to_write, int fd)
+{
+    int test;
+
+    if (to_write)
+    {
+        write_in_hexa((to_write / 256), fd);
+        test = to_write % 256;
+        write(fd, &test, 1);
+    }
+}
+
+int main()
+{
+    int fd;
+
+    fd = open("exemple.txt", O_WRONLY);
+    write_in_hexa(64251, fd);
+    close(fd);
+    return (0);
+}
