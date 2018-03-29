8e8edb9a782dcb7d8b50079670b2014096a8877c
Kevin Nguyen <kenguyen@student.42.fr>
2018-03-29 20:46:04 +0200

    add strendcmp for check .s and .cor in name

--- srcs_vm/vm.c
+++ srcs_vm/vm.c
@@ -6,28 +6,35 @@
 /*   By: ysingaye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 15:56:40 by ysingaye          #+#    #+#             */
-/*   Updated: 2018/03/29 17:24:53 by kenguyen         ###   ########.fr       */
+/*   Updated: 2018/03/29 20:41:15 by kenguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vm.h>
 
-void	check_name(int argc, char **argv)
+void	ft_strendcmp(const char *s1, const char *s2)
 {
-	(void)argc;
-	(void)argv;
-//	int	end;
+	int	i;
 
-//	end = ft_strlen(str) - 1;
-//	if (end > 0 && str[end - 1] == '.' && str[end] == 's')
-//		return ;
-//	else
-//		exit(0);
+	i = ft_strlen(s1) - ft_strlen(s2);
+	if (ft_strcmp(s1 + i, s2))
+		exit(0);
 }
+
 int			main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-//	check_name(argc, argv);
+	int i;
+
+	i = 0;
+	if (argc < 2)
+		exit(0);
+	while (argv[++i])
+	{
+		if (!ft_strcmp(argv[i], "-n"))
+			printf("vm");
+		else
+			ft_strendcmp(argv[i], ".cor");
+	}
+	ft_printf("ok");
 	return (0);
 }
