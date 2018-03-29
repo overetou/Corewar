870b2b36a24a2a98b516a61ff9179d0450aa9d52
Yoan SINGAYE <ysingaye@e1r5p12.42.fr>
2018-03-29 20:48:18 +0200

    update

--- includes/vm.h
+++ includes/vm.h
@@ -6,7 +6,7 @@
 /*   By: kenguyen <kenguyen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/29 16:17:59 by kenguyen          #+#    #+#             */
-/*   Updated: 2018/03/29 17:47:58 by kenguyen         ###   ########.fr       */
+/*   Updated: 2018/03/29 19:58:54 by ysingaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,22 @@
 # include <op.h>
 # include <ncurses.h>
 
+typedef struct			s_player
+{
+	int					nbr;
+	char				*name;
+	char				*comment;
+	int					nbr_live;
+	struct s_player		*next;
+}						t_player;
+
 typedef struct			s_process
 {
 	int					reg[REG_NUMBER];
-	char				*name;
 	char				carry;
 	int					index;
 	int					waitting;
+	int					player_nbr;
 	struct s_process	*next;
 }						t_process;
 
@@ -44,23 +53,23 @@ typedef struct			s_arena
 }						t_arena;
 
 void		ft_live(t_param *param, t_arena *arena, t_process *process);
-void		ft_ld(t_param *param);
-void		ft_st(t_param *param);
-void		ft_add(t_param *param);
-void		ft_sub(t_param *param);
-void		ft_and(t_param *param);
-void		ft_or(t_param *param);
-void		ft_xor(t_param *param);
-void		ft_zjmp(t_param *param);
-void		ft_ldi(t_param *param);
-void		ft_sti(t_param *param);
-void		ft_fork(t_param *param);
-void		ft_lld(t_param *param);
-void		ft_sti(t_param *param);
-void		ft_fork(t_param *param);
-void		ft_lld(t_param *param);
-void		ft_lldi(t_param *param);
-void		ft_lfork(t_param *param);
-void		ft_aff(t_param *param);
+void		ft_ld(t_param *param, t_arena *arena, t_process *process);
+void		ft_st(t_param *param, t_arena *arena, t_process *process);
+void		ft_add(t_param *param, t_arena *arena, t_process *process);
+void		ft_sub(t_param *param, t_arena *arena, t_process *process);
+void		ft_and(t_param *param, t_arena *arena, t_process *process);
+void		ft_or(t_param *param, t_arena *arena, t_process *process);
+void		ft_xor(t_param *param, t_arena *arena, t_process *process);
+void		ft_zjmp(t_param *param, t_arena *arena, t_process *process);
+void		ft_ldi(t_param *param, t_arena *arena, t_process *process);
+void		ft_sti(t_param *param, t_arena *arena, t_process *process);
+void		ft_fork(t_param *param, t_arena *arena, t_process *process);
+void		ft_lld(t_param *param, t_arena *arena, t_process *process);
+void		ft_sti(t_param *param, t_arena *arena, t_process *process);
+void		ft_fork(t_param *param, t_arena *arena, t_process *process);
+void		ft_lld(t_param *param, t_arena *arena, t_process *process);
+void		ft_lldi(t_param *param, t_arena *arena, t_process *process);
+void		ft_lfork(t_param *param, t_arena *arena, t_process *process);
+void		ft_aff(t_param *param, t_arena *arena, t_process *process);
 
 #endif
