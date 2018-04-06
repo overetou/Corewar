9856e4f1209ca967a685ea643451a9a19bf9eae2
Kevin Nguyen <kenguyen@student.42.fr>
2018-04-06 15:16:13 +0200

    replace vm_champs by ressources folder + add some sh test and pdf

---
+++ ressources/tests/errors.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+make -C ../../ && cp ../../asm .
+
+echo "\nUnknown instruction-------"
+./asm ../champs/errors/unknown_instruction.s
+echo "\nWrong param number-------"
+./asm ../champs/errors/bad_param_nb.s
+echo "\nWrong param type-------"
+./asm ../champs/errors/bad_param_type.s
+echo "\nBad character in param-------"
+./asm ../champs/errors/bad_char_in_param.s
+echo "\nBad character in label-------"
+./asm ../champs/errors/bad_char_in_label.s
+echo "\nNonexistent label-------"
+./asm ../champs/errors/nonexistent_label.s
+echo "\nNo code-------"
+./asm ../champs/errors/no_code.s
+echo "\nNo name-------"
+./asm ../champs/errors/no_name.s
+echo "\nNo comment-------"
+./asm ../champs/errors/no_comment.s
+echo "\nName and comment too long-------"
+./asm ../champs/errors/name_com_too_long.s
+
+
