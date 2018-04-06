270414aa4f2df97fb893fdd3358bdf871deb5dd5
Kevin Nguyen <kenguyen@student.42.fr>
2018-04-06 15:24:23 +0200

    forgot .sh test

---
+++ ressources/tests/test.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+make -C ../../ && cp ../../asm .
+
+N=`echo ../champs/Asombra` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/Kappa` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/Machine-gun` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/MarineKing` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/Misaka_Mikoto` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/Rainbow_dash` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/THUNDER` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/Varimathras` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/Wall` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/_` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/_honeybadger` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/bee_gees` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/big_feet` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/big_life` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/bigzork` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/casimir` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/champ` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/corelol` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/darksasuke` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/doge` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/dubo` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/fat_42` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/fluttershy` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/gedeon` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/helltrain` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/jinx` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/justin_bee` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/littlepuppy` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/live` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/loose` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/mandragore` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/meowluigi` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/salamahenagalabadoun` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/sam_2.0` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/seg` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/skynet` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/slider2` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/Octobre_Rouge_V4.2` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/maxidef` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/jumper` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/Gagnant` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/ex` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/Car` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/barriere` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/42` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/mortel` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/terminator` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/turtle` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/ultimate-surrender` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/while_fork` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/youforkmytralala` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+N=`echo ../champs/zork` S=`echo $N.s` COR=`echo $N.cor` && ./asm $S && hexdump $COR > testasm && ./asm42 $S && hexdump $COR > testasm42 && diff testasm testasm42
+#rm ../champs/*.cor
+rm asm
