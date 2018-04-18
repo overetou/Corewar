d0f1e64ca63060458ed799d2ac51683e4a444efe
Yoan SINGAYE <ysingaye@e2r12p21.42.fr>
2018-04-18 17:39:07 +0200

    update diff

--- diff.sh
+++ diff.sh
@@ -2,9 +2,13 @@
 
 let "i = 0"
 let "i = $1"
+#echo "./corewar $2 $3 $4 $5 -dump $i > us"
+#echo "./ressources/corewar $2 $3 $4 $5 -d $i > zaz"
 ./corewar $2 $3 $4 $5 -dump $i > us
 ./ressources/corewar $2 $3 $4 $5 -d $i > zaz
-sed -i '' '1,2d' zaz
+
+sed -i '' '1d' zaz
+sed -i '' '/\*/d' zaz
 sed -i '' '/0x1000 :/d' us
 echo $i
 while [ -z "`diff --suppress-common-lines us zaz`" ]
@@ -14,13 +18,15 @@ do
 		let "i++"
 		./corewar $2 $3 $4 $5 -dump $i > us
 		./ressources/corewar $2 $3 $4 $5 -d $i > zaz
-		sed -i '' '1,2d' zaz
+		sed -i '' '1d' zaz
+		sed -i '' '/\*/d' zaz
 		sed -i '' '/0x1000 :/d' us
 		echo $i
 	done
 	./corewar $2 $3 $4 $5 -dump $i > us
 	./ressources/corewar $2 $3 $4 $5 -d $i > zaz
-	sed -i '' '1,2d' zaz
+	sed -i '' '1d' zaz
+	sed -i '' '/\*/d' zaz
 	sed -i '' '/0x1000 :/d' us
 done
 echo ">>>>>>>>>>>>>>>>>> stop = $i"
