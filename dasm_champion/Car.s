f71e812c962289a0a1cafe27f8afd5d1a854953d
Yoan SINGAYE <ysingaye@e2r9p15.42.fr>
2018-04-27 18:50:38 +0200

    clean

--- dasm_champion/Car.s
+++
@@ -1,6 +0,0 @@
-.name           "Tching tching(Intercepteur), Bouh!Bouh!(bruits d'anti-jeu)"
-.comment        ""
-
-	ld	%400, r3
-	live	%0
-	fork	%-12
--- dasm_champion/seg.s
+++
@@ -1,7 +0,0 @@
-.name           "olag"
-.comment        "lol xptdr"
-
-	sti	r1, 23, r2
-	live	%1
-	live	%2
-	st	r1, 106
